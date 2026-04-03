b091f2d0b65ab9698fbec9f962b8e4f8c64ffd14
sunwoo <sun1008woo@gmail.com>
2026-04-03 23:19:03 +0900

    Fix native app startup order for Android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
+import "react-native-gesture-handler";
+import "react-native-reanimated";
 import "@/global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import "react-native-reanimated";
 import { Platform } from "react-native";
 import "@/lib/_core/nativewind-pressable";
 import { ThemeProvider } from "@/lib/theme-provider";
